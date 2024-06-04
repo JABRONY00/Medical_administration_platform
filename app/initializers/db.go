d71ab9e10bd76ac47f3424093d902f915d84e403
RomaZherko210 <r.zherko@invatechs.com>
2024-06-04 12:57:58 +0300

    change db to pull pq

--- app/initializers/db.go
+++ app/initializers/db.go
@@ -1,55 +1,36 @@
 package initializers
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"os"
-	"os/exec"
 
 	"github.com/JABRONY00/medical_administration_platform/app/helpers"
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5/pgxpool"
 	log "github.com/sirupsen/logrus"
 )
 
-var DB_PORT = helpers.GetEnv("DB_PORT")
-var DB_HOST = helpers.GetEnv("DB_HOST")
-var DB_NAME = helpers.GetEnv("DB_NAME")
-var DB_USER = helpers.GetEnv("DB_USER")
-var DB_PASSWORD = helpers.GetEnv("DB_PASSWORD")
+var (
+	DB_PORT     = helpers.GetEnv("DB_PORT")
+	DB_HOST     = helpers.GetEnv("DB_HOST")
+	DB_NAME     = helpers.GetEnv("DB_NAME")
+	DB_USER     = helpers.GetEnv("DB_USER")
+	DB_PASSWORD = helpers.GetEnv("DB_PASSWORD")
+)
 
-func ConnectDb() *sql.DB {
-	// runMigrations()
-
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+func ConnectDb() *pgxpool.Pool {
+	postgreURL := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	dbpool, err := pgxpool.New(context.Background(), postgreURL)
 	if err != nil {
-		log.Panicf("DB connect err: %v", err)
+		log.Panicf("DB Connection failed: %v", err.Error())
 	}
 
-	err = db.Ping()
+	err = dbpool.Ping(context.Background())
 	if err != nil {
-		log.Panicf("DB ping err: %v", err)
+		log.Panicf("DB Ping failed: %v", err.Error())
 	}
 
-	log.Info("DB was successfully connected!")
-
-	return db
-}
-
-func runMigrations() {
-	psqlInfo := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
-
-	cmd := exec.Command("migrate", "-path", "db/migrations", "-database", psqlInfo, "up")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Run()
-	if err != nil {
-		log.Panicf("Migrations error: %v", err)
-	}
-
-	log.Info("Migrations have passed!")
+	log.Infof("DB connected successfully on port: %v", DB_PORT)
+	return dbpool
 }
