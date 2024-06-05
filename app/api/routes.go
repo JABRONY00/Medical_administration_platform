b1a1e9de271b4209524c796f47f34a961097b4f8
RomaZherko210 <r.zherko@invatechs.com>
2024-06-05 14:59:29 +0300

    fix

--- app/api/routes.go
+++ app/api/routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 
+	"github.com/JABRONY00/medical_administration_platform/app/api/user-service/clients"
 	"github.com/JABRONY00/medical_administration_platform/app/api/user-service/employees"
 )
 
@@ -11,4 +12,5 @@ func Routes(r *gin.Engine, db *pgxpool.Pool) {
 	InitMiddlewares(r)
 
 	employees.Routes(r, db)
+	clients.Routes(r, db)
 }
