8c8b223756422e0a67ba2b64e0a3bea98b266bba
JABRONY00 <mr.lysyna@mail.ru>
2024-06-07 17:35:10 +0300

    new project architecture with client cruds

--- app/api/userMicroservice/controllers/clientControllers.go
+++ app/api/userMicroservice/controllers/clientControllers.go
@@ -1,16 +1,13 @@
 package controllers
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"reflect"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
 
 	"github.com/JABRONY00/medical_administration_platform/app/api/userMicroservice/models"
+	"github.com/JABRONY00/medical_administration_platform/app/api/userMicroservice/services"
 	log "github.com/JABRONY00/medical_administration_platform/app/helpers/log"
 )
 
@@ -37,68 +34,26 @@ func CreateClient(c *gin.Context) {
 			return
 		}
 	}
-
-	client.ID = uuid.New().String()
-
-	client.PasswordHash, err = bcrypt.GenerateFromPassword([]byte(client.Password), 10)
-	if err != nil {
-		log.HttpLog(c, log.Error, http.StatusInternalServerError, "failed to generate password hash")
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-
-	_, err = DB.Exec(context.Background(),
-		"INSERT INTO clients VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
-		client.ID,
-		client.FirstName,
-		client.LastName,
-		client.Phone,
-		client.Email,
-		client.PasswordHash,
-		client.BirthDate,
-		client.Gender,
-		client.Address,
-	)
+	err = services.CreateClient(c, &client)
 	if err != nil {
 		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
 
-	log.HttpLog(c, log.Info, http.StatusOK, fmt.Sprintf("new client created, id=%s", client.ID.String()))
+	log.HttpLog(c, log.Info, http.StatusOK, fmt.Sprintf("new client created, id=%s", client.ID))
 	c.AbortWithStatus(http.StatusOK)
 }
 
 func GetClients(c *gin.Context) {
-	fmt.Println("get clients endpoint")
-	rows, err := DB.Query(context.Background(),
-		"SELECT * FROM clients")
+	queryParams := c.Request.URL.Query()
+	ids := queryParams["id"]
+
+	clients, err := services.GetClients(ids)
 	if err != nil {
 		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
-	}
-	defer rows.Close()
-
-	var clients []models.ClientInfo
-	for rows.Next() {
-		var client models.ClientInfo
-		err := rows.Scan(&client.ID,
-			&client.FirstName,
-			&client.LastName,
-			&client.Phone,
-			&client.Email,
-			&client.PasswordHash,
-			&client.BirthDate,
-			&client.Gender,
-			&client.Address,
-		)
-		if err != nil {
-			log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-		clients = append(clients, client)
 	}
 
 	c.JSON(http.StatusOK, clients)
@@ -106,26 +61,14 @@ func GetClients(c *gin.Context) {
 
 func GetClient(c *gin.Context) {
 	queryParams := c.Request.URL.Query()
-	id, ok := queryParams["id"]
+	ids, ok := queryParams["id"]
 	if !ok {
 		log.HttpLog(c, log.Error, http.StatusBadRequest, "missing id")
 		c.JSON(http.StatusBadRequest, "missing id")
 		return
 	}
 
-	row := DB.QueryRow(context.Background(),
-		"SELECT * FROM clients WHERE id = $1", id[0])
-	var client models.ClientInfo
-	err := row.Scan(&client.ID,
-		&client.FirstName,
-		&client.LastName,
-		&client.Phone,
-		&client.Email,
-		&client.PasswordHash,
-		&client.BirthDate,
-		&client.Gender,
-		&client.Address,
-	)
+	client, err := services.GetClient(&ids[0])
 	if err != nil {
 		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
 		c.AbortWithStatus(http.StatusInternalServerError)
@@ -143,9 +86,7 @@ func DeleteClient(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, "missing id")
 		return
 	}
-
-	_, err := DB.Exec(context.Background(),
-		"DELETE FROM clients WHERE id=$1", id[0])
+	err := services.DeleteClient(&id[0])
 	if err != nil {
 		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
 		c.AbortWithStatus(http.StatusInternalServerError)
@@ -165,73 +106,22 @@ func UpdateClient(c *gin.Context) {
 		return
 	}
 
-	var updClient models.ClientWithPassword
+	var updClient models.ClientInfo
 	err := c.ShouldBindJSON(&updClient)
 	if err != nil {
 		log.HttpLog(c, log.Error, http.StatusBadRequest, "invalid request body")
 		c.JSON(http.StatusBadRequest, "invalid request body")
 		return
 	}
+	updClient.ID = id[0]
 
-	row := DB.QueryRow(context.Background(),
-		"SELECT * FROM clients WHERE id = $1", id[0])
-	var oldClient models.ClientInfo
-	err = row.Scan(&oldClient.ID,
-		&oldClient.FirstName,
-		&oldClient.LastName,
-		&oldClient.Phone,
-		&oldClient.Email,
-		&oldClient.PasswordHash,
-		&oldClient.BirthDate,
-		&oldClient.Gender,
-		&oldClient.Address,
-	)
+	err = services.UpdateClient(&updClient)
 	if err != nil {
 		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
 
-	if updClient.Password != "" {
-		updClient.PasswordHash, err = bcrypt.GenerateFromPassword([]byte(updClient.Password), 10)
-		if err != nil {
-			log.HttpLog(c, log.Error, http.StatusInternalServerError, "failed to generate password hash")
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-	}
-
-	updClientReflection := reflect.ValueOf(&updClient.ClientInfo).Elem()
-	oldClientReflection := reflect.ValueOf(&oldClient).Elem()
-	for i := 1; i < updClientReflection.NumField(); i++ {
-		if updClientReflection.Field(i).IsZero() {
-			updClientReflection.Field(i).Set(oldClientReflection.Field(i))
-		}
-	}
-	updClient.ID = oldClient.ID
-
-	_, err = DB.Exec(context.Background(), "UPDATE clients SET first_name = $1, last_name = $2, phone = $3, email = $4, password_hash = $5, birth_date = $6, gender = $7, address = $8 WHERE id =$9",
-		updClient.FirstName,
-		updClient.LastName,
-		updClient.Phone,
-		updClient.Email,
-		updClient.PasswordHash,
-		updClient.BirthDate,
-		updClient.Gender,
-		updClient.Address,
-		updClient.ID,
-	)
-	if err != nil {
-		log.HttpLog(c, log.Error, http.StatusInternalServerError, err.Error())
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-
-	infoString := fmt.Sprintf("client with id=%s has been updated! Details: %s",
-		oldClient.ID,
-		"expected",
-	)
-
-	log.HttpLog(c, log.Info, http.StatusOK, infoString)
+	log.HttpLog(c, log.Info, http.StatusOK, fmt.Sprintf("Client with id=%s has been updated!", updClient.ID))
 	c.AbortWithStatus(http.StatusOK)
 }
--- app/api/userMicroservice/services/clientServices.go
+++ app/api/userMicroservice/services/clientServices.go
@@ -1,11 +1,75 @@
 package services
 
-func CreateClient() {}
+import (
+	"reflect"
 
-func GetClient() {}
+	"github.com/JABRONY00/medical_administration_platform/app/api/userMicroservice/models"
+	"github.com/JABRONY00/medical_administration_platform/app/api/userMicroservice/query"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
-func GetClients() {}
+func CreateClient(c *gin.Context, client *models.ClientWithPassword) error {
+	//email verification
+	client.ID = uuid.New().String()
+	var err error
+	client.PasswordHash, err = bcrypt.GenerateFromPassword([]byte(client.Password), 10)
+	if err != nil {
+		return err
+	}
 
-func DeleteClient() {}
+	err = query.InsertClient(client)
+	if err != nil {
+		return err
+	}
 
-func UpdateClient() {}
+	return nil
+}
+
+func GetClient(clientID *string) (*models.ClientInfo, error) {
+	client, err := query.GetClient(clientID)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func GetClients(ids []string) ([]models.ClientInfo, error) {
+	var (
+		clients []models.ClientInfo
+		err     error
+	)
+
+	if len(ids) == 0 {
+		clients, err = query.GetAllClients()
+	} else {
+		clients, err = query.GetClientsByIDs(ids)
+	}
+
+	return clients, err
+}
+
+func DeleteClient(id *string) error {
+	err := query.DeleteClient(id)
+	return err
+}
+
+func UpdateClient(updClient *models.ClientInfo) error {
+	oldClient, err := GetClient(&updClient.ID)
+	if err != nil {
+		return err
+	}
+
+	updClientReflection := reflect.ValueOf(&updClient).Elem()
+	oldClientReflection := reflect.ValueOf(&oldClient).Elem()
+	for i := 1; i < updClientReflection.NumField(); i++ {
+		if updClientReflection.Field(i).IsZero() {
+			updClientReflection.Field(i).Set(oldClientReflection.Field(i))
+		}
+	}
+
+	err = query.UpdateClient(updClient)
+
+	return err
+}
